53ea675bc3888e7ca30b62de41a7958bc32884d0
gahyu22@gmail.com <gahyu22@gmail.com>
2021-07-12 20:12:04 +0900

    new

--- P42891.py
+++ P42891.py
@@ -1,28 +1,31 @@
+from collections import deque
+
 def solution(food_times, k):
-    answer = 0
-    ft_len = len(food_times)
     ft = []
-    for i in range(0, ft_len):
+    for i in range(0, len(food_times)):
         ft.append((food_times[i], i))
-    sorted_ft = sorted(ft, reverse=True)
-    while k >= sorted_ft[-1][0] * ft_len:
-        k -= sorted_ft[-1][0] * ft_len
-        sorted_ft.pop()
-
+    sorted_ft = sorted(ft)
+    dq = deque(sorted_ft)
+    rotate = 0
+    while dq:
+        len_dq = len(dq)
+        front = dq.popleft()
+        if front[0] <= rotate:
+            continue
+        fr = front[0]-rotate
+        if len_dq * fr <= k:
+            k -= len_dq * fr
+            rotate += fr
+        else:
+            dq.appendleft(front)
+            break
+    if not dq:
+        return -1
+    sorted_ft = []
+    while dq:
+        front = dq.popleft()
+        sorted_ft.append((front[0]-rotate, front[1]+1))
     sorted_ft = sorted(sorted_ft, key=lambda x: x[1])
-    while 1:
-        top = (sorted_ft[0][0] - 1, sorted_ft[0][1])
-        sorted_ft = sorted_ft[1:-1]
-        if top[0] > 0:
-            sorted_ft.append(top)
-        k -= 1
-        if k == 0:
-            break
-        if not sorted_ft:
-            break
-
-    if sorted_ft:
-        answer = sorted_ft[0][1] + 1
-    else:
-        answer = -1
+    pos = k % len(sorted_ft)
+    answer = sorted_ft[pos][1]
     return answer
--- P72415.py
+++ P72415.py
@@ -1,3 +1,51 @@
+from collections import deque
+
+q = deque()
+def move (a, b, f, board,enter, ctrl, d,):
+    if ctrl == 2 : ctrl = 3
+    dir = [(1,0),(-1,0),(0,1),(0,-1)]
+    x,y = min(3, max(0, a + ctrl * dir[d][0])), min(3, max(0, b + ctrl * dir[d][1]))
+    if a != x or b != y:
+        q.append((x,y,f+1,board,enter))
+
+def is_empty(board):
+    for bd1 in board:
+        for bd2 in bd1:
+            if bd2:
+                return False
+    return True
+
 def solution(board, r, c):
     answer = 0
+    enter = []
+    q.append((r,c,0,board,enter))
+    while q:
+        front = q.popleft()
+        a, b, f, bd,et = front[0], front[1], front[2], front[3], front[4]
+        oet = et
+        obd = bd
+        print(front)
+        if is_empty(bd):
+            answer = f
+            break
+
+        if bd[a][b]:
+            if len(et)==1:
+                r1, c1 = et[0][0], et[0][1]
+                if (r1 != a or c1 != b) and bd[r1][c1] == bd[a][b]:
+                    et = []
+                    bd[r1][c1], bd[a][b] = 0, 0
+                    q.append((a, b, f + 1, bd, et))
+                    bd, et = obd, oet
+            else:
+                et.append((a, b))
+                q.append((a, b, f + 1, bd, et))
+                et = oet
+
+        for i in range(0,4):
+            for j in range(1,3):
+                move(a, b, f, bd,et, j, i)
     return answer
+
+
+solution([[1,0,0,3],[2,0,0,0],[0,0,0,2],[3,0,1,0]],1,0)
