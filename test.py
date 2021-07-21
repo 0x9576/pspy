c2819b7ee76b9a56001253d3afde6324a1bb537e
gahyu22@gmail.com <gahyu22@gmail.com>
2021-07-21 17:18:45 +0900

    new

--- test.py
+++ test.py
@@ -5,13 +5,18 @@ def solution(board):
     q = deque()
     q.append((0, 0, 1))
     q.append((0, 0, 2))
-    board[0][0] = 0
     while q:
         r, c, d = q.popleft()
+        d1, d2 = d - 1, d + 1
+        if d1 < 0:
+            d1 += 4
+        if d2 > 3:
+            d2 -= 4
+        d = [d, d1, d2]
         move = [[-1, 0], [0, 1], [1, 0], [0, -1]]
         for di in d:
             nr, nc = r + move[di][0], c + move[di][1]
-            if di == d:
+            if di == d[0]:
                 next_cost = board[r][c] + 100
             else:
                 next_cost = board[r][c] + 600
@@ -19,4 +24,5 @@ def solution(board):
                 if board[nr][nc] == 0 or board[nr][nc] >= next_cost:
                     board[nr][nc] = next_cost
                     q.append((nr, nc, di))
+    print(board)
     return board[-1][-1]
