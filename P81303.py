df861e2ebc9fcb96aa92cb16fc6b786302c17e57
gahyu22@gmail.com <gahyu22@gmail.com>
2022-02-16 17:03:41 +0900

    new

--- P81303.py
+++ P81303.py
@@ -1,35 +1,73 @@
 def solution(n, k, cmd):
-    lis = [i for i in range(0, n)]
     answer = ''
-    delete_list = []
+    dll = {0: [-1, 1], n-1: [n - 2, -1]}
+    # 양방향 연결리스트
+    # 좌: 현재노드, 우: 이전노드, 다음노드
+    # 이전, 다음 노드가 없다면 -1로 설정한다.
+    deleted_stack = []
+    # 삭제된 노드를 저장하는 스택이다.
+    for i in range(0, n-2):
+        insert(i, i + 2, i + 1, dll)
     for c in cmd:
+        print(dll)
+        print(k)
         if c[0] == "D":
-            num = c[1:]
-            k += int(num)
-        elif c == "C":
-            delete_list.append(lis[k])
-            del lis[k]
-            k = min(k, len(lis) - 1)
+            for _ in range(int(c[1:])):
+                k = dll[k][1]  # 다음노드로 간다.
         elif c[0] == "U":
-            num = c[1:]
-            k -= int(num)
-        else:
-            top = delete_list.pop()
-            if top > lis[-1]:
-                lis.append(top)
-            elif top < lis[0]:
-                lis.insert(0, top)
-                k += 1
+            for _ in range(int(c[1:])):
+                k = dll[k][0]  # 이전 노드로 간다.
+        elif c[0] == "C":
+            ret = delete(k, dll)
+            deleted_stack.append(ret)
+            # 복구를 위해서 스택에 삭제된 노드의 정보를 저장한다.
+            if ret[2] != -1:
+                k = ret[2]  # -1이 아니라면 다음 노드로 변경
             else:
-                for i in range(0, len(lis) - 1):
-                    if lis[i] < top < lis[i + 1]:
-                        lis.insert(i + 1, top)
-                        if i < k:
-                            k += 1
-                        break
-    for i in range(0, n):
-        if i in lis:
-            answer += "O"
+                k = ret[1]  # -1이라면 이전 노드로 변경
         else:
-            answer += "X"
+            node = deleted_stack.pop()
+            restore(node, dll)
+
+    for i in range(n):
+        if i in dll:
+            answer += 'O'
+        else:
+            answer += 'X'
+
     return answer
+
+
+def insert(pre, nex, node, dll):
+    dll[node] = [pre, nex]
+
+
+def delete(k, dll):
+    # k는 삭제되어야 하는 노드다.
+    # 이전 노드와 다음 노드를 dll을 통해서 알아낸다.
+    pre_node = dll[k][0]
+    next_node = dll[k][1]
+    # 이전노드의 다음: 다음노드와 연결
+    if pre_node != -1:
+        dll[pre_node][1] = next_node
+    # 다음 노드의 이전: 이전 노드와 연결
+    if next_node != -1:
+        dll[next_node][0] = pre_node
+    del dll[k]  # 노드삭제
+    # 삭제된 노드정보 반환.
+    return (k, pre_node, next_node)
+
+
+def restore(node, dll):
+    print(node)
+    pre_node = node[1]
+    next_node = node[2]
+    node = node[0]
+    dll[node] = [pre_node, next_node]
+    if pre_node != -1:
+        dll[pre_node][1] = node
+    # 이전 노드의 다음을 복구
+    if next_node != -1:
+        dll[next_node][0] = node
+    # 다음 노드의 이전을 복구
+    return
--- test.py
+++ test.py
@@ -1,45 +1,76 @@
-def solution(board, skill):
-    answer = 0
-    # borad보다 행,열이 1만큼 큰 배열 생성
-    arr = [[0 for c in range(len(board[0])+1)] for r in range(len(board)+1)]
-    for s in skill:
-        point(arr, s)
-    cal(arr)
-    sum_arr(board, arr)
-    for b_list in board:
-        for b in b_list:
-            if b > 0:
-                answer += 1
+def solution(n, k, cmd):
+    answer = ''
+    dll = {0: [-1, 1], n-1: [n - 2, -1]}
+    # 양방향 연결리스트
+    # 좌: 현재노드, 우: 이전노드, 다음노드
+    # 이전, 다음 노드가 없다면 -1로 설정한다.
+    deleted_stack = []
+    # 삭제된 노드를 저장하는 스택이다.
+    for i in range(0, n-2):
+        insert(i, i + 2, i + 1, dll)
+    for c in cmd:
+        print(dll)
+        print(k)
+        if c[0] == "D":
+            for _ in range(int(c[1:])):
+                k = dll[k][1]  # 다음노드로 간다.
+        elif c[0] == "U":
+            for _ in range(int(c[1:])):
+                k = dll[k][0]  # 이전 노드로 간다.
+        elif c[0] == "C":
+            ret = delete(k, dll)
+            deleted_stack.append(ret)
+            # 복구를 위해서 스택에 삭제된 노드의 정보를 저장한다.
+            if ret[2] != -1:
+                k = ret[2]  # -1이 아니라면 다음 노드로 변경
+            else:
+                k = ret[1]  # -1이라면 이전 노드로 변경
+        else:
+            node = deleted_stack.pop()
+            restore(node, dll)
+
+    for i in range(n):
+        if i in dll:
+            answer += 'O'
+        else:
+            answer += 'X'
+
     return answer
 
 
-def point(arr, skill):
-    # 배열의 특정 부분만을 미리 지정 후 나중에 계산
-    # 가중합을 구하면 구하고자 하는 배열이 나온다.
-    if skill[0] == 1:
-        skill[5] = -skill[5]
-    arr[skill[1]][skill[2]] += skill[5]
-    arr[skill[1]][skill[4] + 1] -= skill[5]
-    arr[skill[3]+1][skill[2]] -= skill[5]
-    arr[skill[3]+1][skill[4] + 1] += skill[5]
+def insert(pre, nex, node, dll):
+    dll[node] = [pre, nex]
 
 
-def cal(arr):
-    # 가중합 계산
-    # 왼쪽에서 오른쪽으로 가중합
-    for i in range(0, len(arr)):
-        for j in range(1, len(arr[0])):
-            arr[i][j] += arr[i][j-1]
-    # 위에서 아래로 가중합
-    for j in range(0, len(arr[0])):
-        for i in range(1, len(arr)):
-            arr[i][j] += arr[i-1][j]
+def delete(k, dll):
+    # k는 삭제되어야 하는 노드다.
+    # 이전 노드와 다음 노드를 dll을 통해서 알아낸다.
+    pre_node = dll[k][0]
+    next_node = dll[k][1]
+    # 이전노드의 다음: 다음노드와 연결
+    if pre_node != -1:
+        dll[pre_node][1] = next_node
+    # 다음 노드의 이전: 이전 노드와 연결
+    if next_node != -1:
+        dll[next_node][0] = pre_node
+    del dll[k]  # 노드삭제
+    # 삭제된 노드정보 반환.
+    return (k, pre_node, next_node)
 
 
-def sum_arr(arr, board):
-    # 두개의 배열 요소의 합을 board에 저장
-    for i in range(len(arr)-1):
-        for j in range(len(arr[0])-1):
-            board[i][j] += arr[i][j]
+def restore(node, dll):
+    print(node)
+    pre_node = node[1]
+    next_node = node[2]
+    node = node[0]
+    dll[node] = [pre_node, next_node]
+    if pre_node != -1:
+        dll[pre_node][1] = node
+    # 이전 노드의 다음을 복구
+    if next_node != -1:
+        dll[next_node][0] = node
+    # 다음 노드의 이전을 복구
+    return
 
-solution([[5,5,5,5,5],[5,5,5,5,5],[5,5,5,5,5],[5,5,5,5,5]], [[1,0,0,3,4,4],[1,2,0,2,3,2],[2,1,0,3,1,2],[1,0,1,3,3,1]])
+
+solution(8, 2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z"])
